43acceb9f5372415d579f802aade5488e579ba85
hexin0614 <hexin0614@hotmail.com>
2018-05-12 23:10:27 +0800

    collect all records into a map

--- src/github.com/uguisu/main/mysqlDocumentGenerator.go
+++ src/github.com/uguisu/main/mysqlDocumentGenerator.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 
 	config "github.com/uguisu/config"
@@ -52,22 +53,43 @@ func main() {
 		)
 		checkErr(err)
 
-		log.Printf("tabel_name= %s, table_comment= %s, column_name=%s, collation_name= %s, data_type= %s, character_maximum_length= %s, column_key= %s, is_nullable= %s, column_comment= %s \n",
-			tabelInfoRow.tabel_name,
-			tabelInfoRow.table_comment,
-			tabelInfoRow.column_name,
-			tabelInfoRow.collation_name,
-			tabelInfoRow.data_type,
-			tabelInfoRow.character_maximum_length,
-			tabelInfoRow.column_key,
-			tabelInfoRow.is_nullable,
-			tabelInfoRow.column_comment,
-		)
+		// log.Printf("tabel_name= %s, table_comment= %s, column_name=%s, collation_name= %s, data_type= %s, character_maximum_length= %s, column_key= %s, is_nullable= %s, column_comment= %s \n",
+		// 	tabelInfoRow.tabel_name,
+		// 	tabelInfoRow.table_comment,
+		// 	tabelInfoRow.column_name,
+		// 	tabelInfoRow.collation_name,
+		// 	tabelInfoRow.data_type,
+		// 	tabelInfoRow.character_maximum_length,
+		// 	tabelInfoRow.column_key,
+		// 	tabelInfoRow.is_nullable,
+		// 	tabelInfoRow.column_comment,
+		// )
 
 		tableRecordCollection = append(tableRecordCollection, tabelInfoRow)
 	}
 
 	log.Printf("total = %d", len(tableRecordCollection))
+
+	var lastTableName string = tableRecordCollection[0].tabel_name
+	var lastStartIndex = 0
+	tableMap := make(map[string][]TabelInfo, 0)
+	for i, val := range tableRecordCollection {
+		if lastTableName != val.tabel_name {
+
+			log.Printf("find = %s", val.tabel_name)
+
+			// tabel changed
+			tableMap[lastTableName] = tableRecordCollection[lastStartIndex:i]
+			lastStartIndex = i
+			lastTableName = val.tabel_name
+		}
+	}
+	// tabel changed
+	tableMap[lastTableName] = tableRecordCollection[lastStartIndex:len(tableRecordCollection)]
+
+	for k, v := range tableMap {
+		fmt.Printf("k=%v, v=%v\n", k, v)
+	}
 }
 
 func checkErr(err error) {
